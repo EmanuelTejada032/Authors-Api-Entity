a99d09343c124091a14c1d4014a109da61a5d758
Emanuel De Jesús Tejada Coste <e.coste@sisalril.gov.do>
2022-06-20 08:30:56 -0400

    Add Author controller, Author entitiy, and simple author endpoint to get all hardcoded authors

---
+++ Authors-Api/Authors-Api/Controllers/AuthorController.cs
@@ -0,0 +1,19 @@
+﻿using Authors_Api.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Authors_Api.Controllers
+{
+    [ApiController]
+    [Route("api/Authors")]
+    public class AuthorController: ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Author>> Authors()
+        {
+            return new List<Author>() {
+                new Author() {Id = 1, Name = "Emanuel"},
+                new Author() {Id = 2, Name = "Laudy"}
+            };
+        }
+    }
+}
