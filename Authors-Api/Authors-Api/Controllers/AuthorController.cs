93bc4c18071e2459e386a7faf81f41383dcfb470
Emanuel De Jesús Tejada Coste <e.coste@sisalril.gov.do>
2022-06-23 07:17:17 -0400

    Get, Post , Update, Delete simple endpoints created on Authors Controller

--- Authors-Api/Authors-Api/Controllers/AuthorController.cs
+++ Authors-Api/Authors-Api/Controllers/AuthorController.cs
@@ -1,19 +1,67 @@
 ﻿using Authors_Api.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Authors_Api.Controllers
 {
     [ApiController]
-    [Route("api/Authors")]
+    [Route("api/authors")]
     public class AuthorController: ControllerBase
     {
-        [HttpGet]
-        public ActionResult<List<Author>> Authors()
+        private readonly ApplicationDbContext context;
+
+        public AuthorController(ApplicationDbContext context)
         {
-            return new List<Author>() {
-                new Author() {Id = 1, Name = "Emanuel"},
-                new Author() {Id = 2, Name = "Laudy"}
-            };
+            this.context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<Author>>> Get()
+        {
+            return await context.Authors.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Author author)
+        {
+            context.Add(author);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(int id , Author author)
+        {
+            if(author.Id != id)
+            {
+                return BadRequest("Url id does not match author id");
+            }
+
+            bool exists = await context.Authors.AnyAsync(x => x.Id == id);
+            if (!exists)
+            {
+                return NotFound($"Author with id: {id} doesn\'t exists");
+            }
+
+            context.Update(author);
+            await context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            bool exists = await context.Authors.AnyAsync(x => x.Id == id);
+            if(!exists)
+            {
+                return NotFound($"Author with id { id } doesn\'t exists");
+            }
+
+            context.Remove(new Author() {Id = id });
+            await context.SaveChangesAsync();
+            return Ok();
         }
     }
 }
